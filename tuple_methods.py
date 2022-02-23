c12fb16249d2d95facd2ba1f93eb58fac1aa2b3f
Destroid <97206601+Destroid1669@users.noreply.github.com>
2022-02-23 19:25:24 +0530

    refactored code

--- tuple_methods.py
+++ tuple_methods.py
@@ -1,14 +1,14 @@
-r"""This is Re-write of tuple methods """
+r"This is Re-write of tuple methods"
 
 # these are two tuple methods in python
 __all__ = ["count", "index"]
 
-def errorhandler(self):
-    """Raises Type error according to the arguments passed to it"""
+def errorhandler(*args):
+    "Raises Type error according to the arguments passed to it"
 
-    for i in self:
-        if not isinstance(self[i], i):
-            raise TypeError("expected {} found {}".format(i.__name__, type(self[i]).__name__))
+    for value, datatype in args:
+        if not isinstance(value, datatype):
+            raise TypeError("expected {} found {}".format(datatype.__name__, type(value).__name__))
 
 """/*
 # A sort Note about these methods.
@@ -23,63 +23,53 @@ whereas substring is compared with same length of string slice iterations.
 */"""
 
 def count(self, sub, start = 0, end = None):
-    """count(self, sub[, start[, end]]) -> int
-    Returns the number of non-overlapping occurrences of substring sub in string self[start:end].
+    """Returns the number of non-overlapping occurrences of substring sub in string self[start:end].
     Optional arguments start and end are interpreted as in slice notation."""
 
-    errorhandler({int: start})
+    errorhandler([start, int])
     if end is not None and not isinstance(end, int):
-        errorhandler({int: end})
+        errorhandler([end, int])
+    iter(self) # Raises error for non iterables
     
     iterable = self[start: end]
-    try:
-        iter(self) # checking for iterable object
-        # raising `AssertionError` if not found `str`
-        assert isinstance(self, str)
-        # issue with counting empty string
-        # returns wrong result for this case
+    if isinstance(self, str):
+        #! issue with counting empty string
+        #! returns wrong result for this case
         Count, len_sub = 0, len(sub)
         for i in range(len(iterable)):
             if sub == iterable[i: i+len_sub]:
                 Count += 1
         return Count if sub != "" else Count+1
-    except AssertionError:
+    else:
         # expecting for iterable object
         Count = 0
         for i in iterable:
             if sub == i:
                 Count += 1
         return Count
-    except:
-        raise # Raising exception if not found iterable
 
 def index(self, sub, start = 0, end = None):
-    """index(self, sub[, start[, end]]) -> int
-    Returns the lowest index in self where substring sub is found,
+    """Returns the lowest index in self where substring sub is found,
     such that sub is contained within self[start:end].  Optional
     arguments start and end are interpreted as in slice notation.
     Raises ValueError when the substring is not found."""
 
-    errorhandler({int: start})
+    errorhandler([start, int])
     if end is not None and not isinstance(end, int):
-        errorhandler({int: end})
+        errorhandler([end, int])
     if sub == "":
         return 0
+    iter(self) # Raises error for non iterables
 
     iterable = self[start: end]
-    try:
-        iter(self) # checking for iterable object
-        # raising `AssertionError` if not found `str`
-        assert isinstance(self, str)
+    if isinstance(self, str):
         len_sub = len(sub)
         for i in range(len(self)):
             if sub == iterable[i: i+len_sub]:
                 return i
-    except AssertionError:
+    else:
         # expecting for iterable object
         for i in range(len(iterable)):
             if sub == iterable[i]:
                 return i
-    except:
-        raise # Raising exception if not found iterable
     raise ValueError("%s substring not found" % sub)

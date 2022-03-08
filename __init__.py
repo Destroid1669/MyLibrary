fe07be0640ac7cb28eb63c06ffc4e5d828c2b428
Destroid <97206601+Destroid1669@users.noreply.github.com>
2022-03-08 22:52:47 +0530

    Revamped code
    This PR consists of many changes of new fixes, improvements and file additions.

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from Python_functions_and_methods.data_types import *
+del Bool; del Range # removing duplicate functions
+from Python_functions_and_methods.string_methods import *
+from Python_functions_and_methods.built_in_functions import *
